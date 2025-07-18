5213ae268406849234fac99f6e747fc0fff40a1a
Quesia Araújo <quesia.exe@gmail.com>
2025-07-18 14:28:15 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Te amo meu nenem</h1>
+function Home() {
+  return <h1>Te amo meu nenem</h1>;
+}
+
+function test() {
+  console.log("Identação errada");
 }
 
 export default Home;
